82af68bd622e055a6733ac5d594fbbaaca83b882
jeffgat <jeff.gtbn@gmail.com>
2021-02-21 09:35:10 -0800

    covered quick sort

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const merge = require('./merge-sort');
+const quick = require('./quick-sort');
 
 const port = 4000;
 const app = express();
 
-merge.mergeSort([10, 24, 76, 73]);
+// merge.mergeSort([10, 24, 76, 73]);
+quick.quickSort([4, 8, 2, 1, 5, 7, 6, 3])
+
 
 app.listen(port),
   () => {
---
+++ quick-sort.js
@@ -0,0 +1,35 @@
+function pivot(arr, start = 0, end = arr.length + 1) {
+  function swap(array, i, j) {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  let pivot = arr[start];
+  let swapIndex = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIndex++;
+      swap(arr, swapIndex, i);
+    }
+  }
+  swap(arr, start, swapIndex);
+  return swapIndex;
+}
+
+// quick.pivot([4, 8, 2, 1, 5, 7, 6, 3]);
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    // left
+    quickSort(arr, left, pivotIndex - 1);
+    // right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  console.log(arr);
+  return arr;
+}
+
+exports.quickSort = quickSort;
+exports.pivot = pivot;
