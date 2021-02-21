df4c66ad61a4ae4c79ecd7b9ab4db11bbe379d95
jeffgat <jeff.gtbn@gmail.com>
2021-02-21 10:08:47 -0800

    covered radix sort

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const merge = require('./merge-sort');
 const quick = require('./quick-sort');
+const radix = require('./radix-sort')
 
 const port = 4000;
 const app = express();
 
 // merge.mergeSort([10, 24, 76, 73]);
-quick.quickSort([4, 8, 2, 1, 5, 7, 6, 3])
+// quick.quickSort([4, 8, 2, 1, 5, 7, 6, 3])
+radix.radixSort([23, 345, 5467, 12, 2345, 9852])
 
 
 app.listen(port),
