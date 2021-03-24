59e1ead98844139786b1983c4b684ba616007615
jeffgat <jeff.gtbn@gmail.com>
2021-03-24 10:48:30 -0600

    queues and binary search trees

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const quick = require('./quick-sort');
 const radix = require('./radix-sort');
 const nodes = require('./doubly-linked-list');
 const stack = require('./stack');
+const tree = require('./binary-search-tree');
 
 const port = 6000;
 const app = express();
@@ -12,6 +13,9 @@ const app = express();
 // quick.quickSort([4, 8, 2, 1, 5, 7, 6, 3])
 // radix.radixSort([23, 345, 5467, 12, 2345, 9852])
 // let list = new nodes.DoublyLinkedList();
+let t = new tree.BinarySearchTree()
+
+console.log(t.insert(10));
 
 
 app.listen(port),
