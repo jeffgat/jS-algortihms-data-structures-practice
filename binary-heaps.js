2524c2a9eba525cfaab99b6b4e6cf036731fbd8c
jeffgat <jeff.gtbn@gmail.com>
2021-04-05 10:39:59 -0400

    implemented remove function on heaps

--- binary-heaps.js
+++ binary-heaps.js
@@ -12,9 +12,54 @@ class MaxBinaryHeap {
     while (true) {
       let parentIdx = Math.floor((idx - 1) / 2);
       if (element <= parent) break;
-      
+
       this.values[parentIdx] = element;
       this.values[idxparentIdx] = parent;
+      idx = parentIdx;
+    }
+  }
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+
+    this.values[0] = end;
+    this.sikDown();
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx - 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+        if (rightChild > element) {
+          rightChild = this.values[rightChildIdx];
+          swap = rightChildIdx;
+          if (
+            (swap === null && rightChild > element) ||
+            (swap !== null && rightChild > leftChild)
+          ) {
+            swap = rightChildIdx;
+          }
+        }
+      }
+
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
       idx = parentIdx;
     }
   }
